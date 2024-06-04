a102ea08c319b5122dd8b7acbecc0e4ba681e8bc
Noah Seltzer <noahseltzer@outlook.com>
2024-06-04 15:12:07 -0700

    init: repo setup and canvas with fabric

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,28 @@
+'use client'
 import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
 
+import { fabric } from 'fabric'
 export default function Home() {
+
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+  const [fabricCanvas, setFabricCanvas] = useState<fabric.Canvas | Falsy>(null)
+
+  useEffect(() => {
+    if (!canvasRef.current) return
+    const canvas = new fabric.Canvas(canvasRef.current)
+    setFabricCanvas(canvas)
+
+    const rect = new fabric.Rect({
+      left: 100,
+      top: 100,
+      fill: 'red',
+      width: 20,
+      height: 20
+    });
+
+    canvas.add(rect)
+  }, [canvasRef.current])
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -28,7 +50,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+      {/* <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/next.svg"
@@ -37,7 +59,9 @@ export default function Home() {
           height={37}
           priority
         />
-      </div>
+      </div> */}
+
+      <canvas ref={canvasRef} width='500' height='500'></canvas>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a
---
+++ src/types/global-types.ts
@@ -0,0 +1,5 @@
+type Falsy = false | '' | 0 | 0n | null | undefined
+
+type Truthy<T> = T extends Falsy ? never : T
+
+type Nullish = null | undefined
